01e41ec5e6c9b9c6c402ca634a5c03adda60b27c
JasminGloth <113904560+JasminGloth@users.noreply.github.com>
2024-08-10 23:31:06 +0200

    ..

--- src/components/BedarfsrechnerPR3.tsx
+++ src/components/BedarfsrechnerPR3.tsx
@@ -2,8 +2,8 @@ import waren from '../data/portRoyale3/waren.json';
 import { useState} from 'react';
 
 export const BedarfsrechnerPR3 = () => {
-    const tage = useState(0);
-    const anzahl = useState(0);
+    const [anzahl, setAnzahl] = useState(0);
+	const [tage, setTage] = useState(0);
 
     const ergebnisse = waren.map((ware) => {
         const bedarf = Math.round(anzahl * ware.bedarf *tage);
