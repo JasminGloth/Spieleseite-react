c745e5df9b9ce0939018baa5395a4ebdd6103f9d
JasminGloth <113904560+JasminGloth@users.noreply.github.com>
2024-09-19 11:21:23 +0200

    schiffseite angepasst

--- src/pages/PageSchiffePR3.tsx
+++ src/pages/PageSchiffePR3.tsx
@@ -1,7 +1,17 @@
+import schiffe from "../data/portRoyale3/schiffe.json";
+
 export const PageSchiffePR3 = () => {
 	return (
 		<div className="page pageSchiffePR3">
-			<p>This is the SchiffePR3 page.</p>
+			<h2>Schiffe</h2>
+			<div className="shipgrid">
+				{schiffe.map((schiff) => {
+					return <div>
+					<div>{schiff.name}</div>
+					<div><img src={schiff.bild} alt="" /></div>
+					</div>
+				})}
+			</div>
 		</div>
 	);
 };
